1b6fa40d5224f85f7f4026749d8abddb9dd3e163
viniciusoliveiras <vinitag190@gmail.com>
2021-01-25 20:43:00 -0300

    Aula 01

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,59 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import db from "../db.json";
+import Widget from "../src/components/Widget";
+import Footer from "../src/components/Footer";
+import GithubCorner from "../src/components/GithubCorner";
+import QuizBackground from "../src/components/QuizBackground";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center;
+//   background-repeat: no-repeat;
+// `;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+
+  @media screen and (max-width: 550px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>{db.title}</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>{db.description}</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Header>
+            <h1>Quizes da Galera</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              Scelerisque varius morbi enim nunc. Id neque aliquam vestibulum
+              morbi blandit cursus risus at ultrices. Purus in massa tempor nec
+              feugiat nisl. In hac habitasse platea dictumst.
+            </p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GithubCorner projectUrl="https://github.com/viniciusoliveiras" />
+    </QuizBackground>
+  );
 }
