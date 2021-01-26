b5a22ba34351989445ee62440a64af83596cd7e1
viniciusoliveiras <vinitag190@gmail.com>
2021-01-25 23:12:25 -0300

    Meta tags

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Widget from "../src/components/Widget";
 import Footer from "../src/components/Footer";
 import GithubCorner from "../src/components/GithubCorner";
 import QuizBackground from "../src/components/QuizBackground";
+import Head from "next/head";
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -12,6 +13,49 @@ import QuizBackground from "../src/components/QuizBackground";
 //   background-position: center;
 //   background-repeat: no-repeat;
 // `;
+
+export function IndexPage() {
+  return (
+    <div>
+      <Head>
+        {/*  Primary Meta Tags  */}
+        <title>Vini Quiz</title>
+        <meta name="title" content="Vini Quiz" />
+        <meta
+          name="description"
+          content="Website criado durante a Imersão React v2 da Alura."
+        />
+
+        {/*  Open Graph / Facebook  */}
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:url"
+          content="https://imersaoreact-aluraquiz.viniciusoliveiras.vercel.app/"
+        />
+        <meta property="og:title" content="Vini Quiz" />
+        <meta
+          property="og:description"
+          content="Website criado durante a Imersão React v2 da Alura."
+        />
+        <meta property="og:image" content="" />
+
+        {/*  Twitter  */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://imersaoreact-aluraquiz.viniciusoliveiras.vercel.app/"
+        />
+        <meta property="twitter:title" content="Vini Quiz" />
+        <meta
+          property="twitter:description"
+          content="Website criado durante a Imersão React v2 da Alura."
+        />
+        <meta property="twitter:image" content="" />
+      </Head>
+      <p>Hello world!</p>
+    </div>
+  );
+}
 
 export const QuizContainer = styled.div`
   width: 100%;
